95ab188778be4da82a2b215e924db1b6fb3b88ef
Jessica Lörs <jessica.loers@gmail.com>
2025-01-07 15:22:50 +0100

    add demo code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import "./styles.css";
+import './styles.css';
+import { useState } from 'react';
 
 export default function App() {
-  return <Counter />;
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
 }
 
 function Counter() {
+  // state variable , setter function - inital value
+  const [count, setCount] = useState(0);
+  const [isDarkModeOn, setIsDarkModeOn] = useState(false);
+
+  console.log('DARK_', isDarkModeOn);
+
+  // let count = 123;
+  // console.log('_Count_outer', count);
+
+  function handleClick() {
+    // count = count + 1;
+    setCount(count + 1);
+    // console.log('_Count', count);
+  }
+
   return (
-    <div className="counter">
-      <p>You have clicked 0 times</p>
-      <button>Click me</button>
+    <div className={isDarkModeOn ? 'counter dark' : 'counter'}>
+      <p>You have clicked {count} times</p>
+      <button onClick={handleClick}>Click me</button>
+      <br />
+      <button onClick={() => setIsDarkModeOn(!isDarkModeOn)}>Dark Mode</button>
     </div>
   );
 }
